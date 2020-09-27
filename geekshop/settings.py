a8ef9a13a8168d33cf1f0ef9e3148f6cfbad0b68
jasja1 <jasja1@yandex.ru>
2020-09-27 23:40:11 +0300

    ready homework (lesson-3)

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': ''
     }
 }
 
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
